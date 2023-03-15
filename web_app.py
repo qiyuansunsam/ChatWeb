5a6d44e4c2e990291423e64d4bedea43fdfff7af
qsun0008 <qsun0008@uni.sydney.edu.au>
2023-03-15 13:56:01 +1100

    login

--- web_app.py
+++ web_app.py
@@ -1,15 +1,31 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import check_password_hash
 import json
 import openai
 import os
 
 openai.api_key = "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"
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "index"
 
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+        
 users = {
     "user1": "password1",
     "user2": "password2"
 }
+
+@login_manager.user_loader
+def load_user(username):
+    if username in users:
+        return User(username)
+    return None
 
 @app.route('/')
 def index():
@@ -19,16 +35,27 @@ def index():
 def login():
     username = request.form.get('username')
     password = request.form.get('password')
+    next_url = request.args.get('next') or url_for('messaging')
     if username in users and users[username] == password:
-        return redirect(url_for('messaging'))
+        user = User(username)
+        login_user(user)
+        return redirect(next_url)
     else:
-        return redirect(url_for('messaging'))
+        return redirect(url_for('index'))
+    
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/messaging')
+@login_required
 def messaging():
     return render_template('messaging.html')
 
 @app.route('/send-message', methods=['POST'])
+@login_required
 def send_message():
     message = request.json.get('message')
     messages = [
